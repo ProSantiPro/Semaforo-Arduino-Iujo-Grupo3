dc6ed122d8956413f510043b20a972d21bf5ad1a
ProSantiPro <strb2006@gmail.com>
2026-08-02 12:52:42 -0400

    Cambios de variables y fase1

--- equipo31.ino
+++ equipo31.ino
@@ -1,61 +1,37 @@
-// --- definicion de pines ---
+// C++ code
+//
+const int pinGrupoA_1 = 5;
+const int pinGrupoA_2 = 6;
+const int pinGrupoA_3 = 7;
+const int pinGrupoB_1 = 4;
+const int pinGrupoB_2 = 3;
+const int pinGrupoC_1 = 10;
+const int pinGrupoC_2 = 9;
+const int pinGrupoD_1 = 11;
+const int pinGrupoD_2 = 12;
+const int pinGrupoD_3 = 13;
 
-// semaforo vehicular este/oeste y oeste/este (cables verdes)
-// al estar conectados en paralelo, un solo pin controla ambos lados
-const int vehiculoEORojo = 13;
-const int vehiculoEOAmarillo = 12;
-const int vehiculoEOVerde = 11;
-
-// semaforo peatonal este/oeste y oeste/este (cables celestes)
-const int peatonalEORojo = 10;
-const int peatonalEOVerde = 9;
-
-// semaforo vehicular norte/sur y sur/norte (cables marrones)
-const int vehiculoNSRojo = 8;
-const int vehiculoNSAmarillo = 7;
-const int vehiculoNSVerde = 6;
-
-// semaforo peatonal norte/sur y sur/norte (cables naranjas)
-const int peatonalNSRojo = 5;
-const int peatonalNSVerde = 4;
-
-// --- tiempos (en milisegundos) ---
-const int tiempoVerde = 10000;   // 10 segundos
-const int tiempoAmarillo = 5000; // 5 segundos
-
-void setup() {
-  // configurar todos los pines como salidas
-  pinMode(vehiculoEORojo, OUTPUT);
-  pinMode(vehiculoEOAmarillo, OUTPUT);
-  pinMode(vehiculoEOVerde, OUTPUT);
-  
-  pinMode(peatonalEORojo, OUTPUT);
-  pinMode(peatonalEOVerde, OUTPUT);
-  
-  pinMode(vehiculoNSRojo, OUTPUT);
-  pinMode(vehiculoNSAmarillo, OUTPUT);
-  pinMode(vehiculoNSVerde, OUTPUT);
-  
-  pinMode(peatonalNSRojo, OUTPUT);
-  pinMode(peatonalNSVerde, OUTPUT);
+void setup()
+{
+  // Ampliado hasta el pin 13 para incluir los pines 12 y 13 que estás utilizando
+  for (int i = 2; i <= 13; i++){
+    pinMode(i, OUTPUT);
+  }
 }
 
-void loop() {
-  // ==========================================
-  // fase 1: paso vehicular este/oeste
-  // ==========================================
-  // vehiculos e/o avanzan, peatones e/o esperan
-  digitalWrite(vehiculoEORojo, LOW);
-  digitalWrite(vehiculoEOVerde, HIGH);
-  digitalWrite(peatonalEORojo, HIGH);
-  digitalWrite(peatonalEOVerde, LOW);
+void loop()
+{
+  // FASE 1
+  digitalWrite(pinGrupoA_1, LOW);
+  digitalWrite(pinGrupoA_2, LOW);
+  digitalWrite(pinGrupoA_3, HIGH);
+  digitalWrite(pinGrupoB_1, LOW);
+  digitalWrite(pinGrupoB_2, HIGH);
+  digitalWrite(pinGrupoC_1, HIGH);
+  digitalWrite(pinGrupoC_2, LOW);
+  digitalWrite(pinGrupoD_1, HIGH);
+  digitalWrite(pinGrupoD_2, LOW);
+  digitalWrite(pinGrupoD_3, LOW);
   
-  // vehiculos n/s esperan, peatones n/s cruzan
-  digitalWrite(vehiculoNSVerde, LOW);
-  digitalWrite(vehiculoNSRojo, HIGH);
-  digitalWrite(peatonalNSRojo, LOW);
-  digitalWrite(peatonalNSVerde, HIGH);
-  
-  delay(tiempoVerde);
-
+  delay(5000);
 }
